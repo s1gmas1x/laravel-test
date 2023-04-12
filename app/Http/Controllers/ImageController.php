438ce2fc003d74d1ff277e235a56d6ee282dbdd7
Chad Kohl <v-chadkohl@microsoft.com>
2023-04-12 16:13:23 -0600

    added models, migrated DB

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MessageController extends Controller
+class ImageController extends Controller
 {
     /**
      * Display a listing of the resource.
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MessageController extends Controller
+class PostController extends Controller
 {
     /**
      * Display a listing of the resource.

ecaa56724f76e7bfa9a95a4705e4fdf542fd5bff
Chad Kohl <v-chadkohl@microsoft.com>
2023-04-13 13:30:14 -0600

    added inertia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite('resources/js/app.js')
     @inertiaHead
-
   </head>
   <body>
     @inertia

b09253ace7734f5b62cf97bece85a903b5001f25
Chad Kohl <v-chadkohl@microsoft.com>
2024-09-05 13:27:45 -0600

    fixed layout

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -67,95 +67,75 @@ const fetchRandomUserWithPosts = async (seconds) => {
 
 
 <template>
+
   <Head title="Welcome" />
 
   <div
-    class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-gray-900 bg-[url('@/img/test.svg')] selection:bg-red-500 selection:text-white"
-  >
+    class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-gray-900 bg-[url('@/img/test.svg')] selection:bg-red-500 selection:text-white">
     <div v-if="canLogin" class="p-6 text-right sm:fixed sm:top-0 sm:right-0">
-      <Link
-        v-if="$page.props.auth.user"
-        :href="route('dashboard')"
-        class="font-semibold text-gray-400 hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-        >Dashboard</Link
-      >
+      <Link v-if="$page.props.auth.user" :href="route('dashboard')"
+        class="font-semibold text-gray-400 hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+      Dashboard</Link>
 
       <template v-else>
-        <Link
-          :href="route('login')"
-          class="font-semibold text-gray-400 hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-          >Log in</Link
-        >
+        <Link :href="route('login')"
+          class="font-semibold text-gray-400 hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+        Log in</Link>
       </template>
     </div>
 
     <div class="p-6 mx-auto max-w-7xl lg:p-8">
       <div class="flex justify-center">
         <Link href="/">
-          <ApplicationLogo class="w-40 h-40 text-red-600" />
+        <ApplicationLogo class="w-40 h-40 text-red-600" />
         </Link>
       </div>
 
       <div class="">
-        <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:gap-10">
+        <div class="grid grid-cols-1 gap-16 md:grid-cols-2 lg:gap-64">
           <div
-            class="flex p-6 transition-all scale-100 rounded-lg shadow-2xl h-44 bg-gray-800/50 backdrop-blur-sm bg-gradient-to-bl from-gray-700/50 via-transparentring-1 ring-inset ring-white/5 shadow-gray-500/20 dark:shadow-none duration-250 focus:outline focus:outline-2 focus:outline-red-500"
-          >
+            class="flex p-6 transition-all scale-100 rounded-lg shadow-2xl h-44 bg-gray-800/50 backdrop-blur-sm bg-gradient-to-bl h-fit from-gray-700/50 via-transparentring-1 ring-inset ring-white/5 shadow-gray-500/20 dark:shadow-none duration-250 focus:outline focus:outline-2 focus:outline-red-500">
             <div class="flex flex-col justify-between w-full gap-4">
               <div>
                 <h2 class="text-xl font-semibold text-gray-900 dark:text-white">
                   Response Time
                 </h2>
 
-                <p
-                  class="mt-4 text-sm leading-relaxed text-gray-500 dark:text-gray-400"
-                >
+                <p class="mt-4 text-sm leading-relaxed text-gray-500 dark:text-gray-400">
                   Click one of the buttons to simulate long response times.
                 </p>
               </div>
-              <div
-                class="flex flex-row flex-wrap justify-between w-full gap-4 mt-4"
-              >
+              <div class="flex flex-row flex-wrap justify-between w-full gap-4 mt-4">
                 <div>
-                  <PrimaryButton
-                    v-bind:class="
-                      loading
-                        ? 'bg-gray-600  outline-none ring-2 ring-indigo-500 ring-offset-2'
-                        : 'bg-gray-800'
-                    "
-                    @click="fetchRandomUserWithPosts(15)"
-                    ><p>15 seconds</p></PrimaryButton
-                  >
+                  <PrimaryButton v-bind:class="loading
+                    ? 'bg-gray-600  outline-none ring-2 ring-indigo-500 ring-offset-2'
+                    : 'bg-gray-800'
+                    " @click="fetchRandomUserWithPosts(15)">
+                    <p>15 seconds</p>
+                  </PrimaryButton>
                 </div>
                 <div>
-                  <PrimaryButton
-                    v-bind:class="
-                      loading
-                        ? 'bg-gray-600  outline-none ring-2 ring-indigo-500 ring-offset-2'
-                        : 'bg-gray-800'
-                    "
-                    @click="fetchRandomUserWithPosts(30)"
-                    ><p>30 seconds</p></PrimaryButton
-                  >
+                  <PrimaryButton v-bind:class="loading
+                    ? 'bg-gray-600  outline-none ring-2 ring-indigo-500 ring-offset-2'
+                    : 'bg-gray-800'
+                    " @click="fetchRandomUserWithPosts(30)">
+                    <p>30 seconds</p>
+                  </PrimaryButton>
                 </div>
                 <div>
-                  <PrimaryButton
-                    v-bind:class="
-                      loading
-                        ? 'bg-gray-600  outline-none ring-2 ring-indigo-500 ring-offset-2'
-                        : 'bg-gray-800'
-                    "
-                    @click="fetchRandomUserWithPosts(90)"
-                    ><p>90 seconds</p></PrimaryButton
-                  >
+                  <PrimaryButton v-bind:class="loading
+                    ? 'bg-gray-600  outline-none ring-2 ring-indigo-500 ring-offset-2'
+                    : 'bg-gray-800'
+                    " @click="fetchRandomUserWithPosts(90)">
+                    <p>90 seconds</p>
+                  </PrimaryButton>
                 </div>
               </div>
             </div>
           </div>
 
           <div
-            class="flex p-6 transition-all scale-100 rounded-lg shadow-2xl h-80 bg-gray-800/50 bg-gradient-to-bl backdrop-blur-sm from-gray-700/50 via-transparent ring-1 ring-inset ring-white/5 shadow-gray-500/20 dark:shadow-none duration-250 focus:outline focus:outline-2 focus:outline-red-500"
-          >
+            class="flex p-6 transition-all scale-100 rounded-lg shadow-2xl h-fit bg-gray-800/50 bg-gradient-to-bl backdrop-blur-sm from-gray-700/50 via-transparent ring-1 ring-inset ring-white/5 shadow-gray-500/20 dark:shadow-none duration-250 focus:outline focus:outline-2 focus:outline-red-500">
             <div class="flex flex-col justify-between w-full gap-4">
               <div>
                 <h2 class="text-xl font-semibold text-white">
@@ -164,26 +144,11 @@ const fetchRandomUserWithPosts = async (seconds) => {
                 <!-- Conditional Rendering for Spinner -->
                 <template v-if="loading">
                   <!-- Spinner -->
-                  <div
-                    class="flex flex-col items-center justify-center text-white"
-                  >
-                    <svg
-                      class="transition-all duration-200 h-44 animate-spin"
-                      viewBox="0 0 800 800"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <circle
-                        opacity="0.25"
-                        cx="400"
-                        cy="400"
-                        fill="none"
-                        r="200"
-                        stroke-width="15"
-                        stroke="#dc2626"
-                        stroke-dasharray="700 1400"
-                        stroke-linecap="round"
-                        stroke-dashoffset="0"
-                      />
+                  <div class="flex flex-col items-center justify-center text-white">
+                    <svg class="transition-all duration-200 h-44 animate-spin" viewBox="0 0 800 800"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <circle opacity="0.25" cx="400" cy="400" fill="none" r="200" stroke-width="15" stroke="#dc2626"
+                        stroke-dasharray="700 1400" stroke-linecap="round" stroke-dashoffset="0" />
                     </svg>
 
                     <p>Loading...</p>
@@ -192,11 +157,7 @@ const fetchRandomUserWithPosts = async (seconds) => {
                 <template v-else>
                   <div class="flex flex-col justify-between">
                     <div>
-                      <h4
-                        v
-                        v-if="randomUser.name"
-                        class="mt-4 leading-relaxed text-gray-200"
-                      >
+                      <h4 v v-if="randomUser.name" class="mt-4 leading-relaxed text-gray-200">
                         {{ randomUser.name }}
                       </h4>
                       <p class="leading-relaxed text-gray-400">
@@ -220,15 +181,10 @@ const fetchRandomUserWithPosts = async (seconds) => {
               </div>
             </div>
           </div>
-
-        
-
         </div>
       </div>
 
-      <div
-        class="flex justify-center px-6 mt-16 sm:items-center sm:justify-between"
-      ></div>
+      <div class="flex justify-center px-6 mt-16 sm:items-center sm:justify-between"></div>
 
       <div class="ml-4 text-sm text-center text-gray-400 sm:text-right sm:ml-0">
         Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
